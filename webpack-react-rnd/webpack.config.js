61b68e12a679622a00ac335186c53493a2faa8cb
Prakash Rai <prakash.bahing21@gmail.com>
2023-02-10 20:37:19 +0545

    Webpack setup

---
+++ webpack-react-rnd/webpack.config.js
@@ -0,0 +1,34 @@
+const port = process.env.PORT || 8000;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "main.js",
+    //If you want to create unique bundle file everytime you build the project then you can use fullhash like this
+    // filename:"main.[fullhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    historyApiFallback: true,
+    open: true,
+  },
+};
