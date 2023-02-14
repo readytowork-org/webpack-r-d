f24cc1e768d898a11db47940727577f03438cc56
Prakash Rai <prakash.bahing21@gmail.com>
2023-02-14 14:02:27 +0545

    Feat: Successful build and running

---
+++ webpack-react-rnd/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        fontSize: "22px",
+        background: "green",
+        minHeight: "100vh",
+      }}
+    >
+      <p>Hello world. This is homepage.</p>
+    </div>
+  );
+}
+
+export default App;
--- webpack-react-rnd/webpack.config.js
+++ webpack-react-rnd/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "main.js",
